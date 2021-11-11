2b9fb8b59653d83ccff2c269f41eb198a67298cd
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-11 15:46:05 +0530

    normal status controller

---
+++ spec/requests/status_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+
+RSpec.describe "Status requests" do
+    describe "Get /status" do
+        it 'returns a status message' do
+            get('/status')
+            json = JSON.parse(response.body)
+            expect(json['status']).to eql('ok')
+        end
+    end
+end
