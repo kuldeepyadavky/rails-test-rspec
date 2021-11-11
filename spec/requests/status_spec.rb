14633ca1b3b409155d2cc5274328bb6808b1dfc3
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-11 16:42:58 +0530

    rspec 6

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 
 RSpec.describe "Status requests" do
-    describe "Get /status" do
+    describe "Get /api/status" do
         it 'returns a status message' do
-            get('/status')
+            get('/api/status')
             json = JSON.parse(response.body)
             expect(json['status']).to eql('ok')
             expect(response.status).to eql(200)
