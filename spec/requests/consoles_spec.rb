4d9cfa1a7fabce620efcd9dd5f39dad6bd0698a5
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-11 15:55:02 +0530

    rspec 3

---
+++ spec/requests/consoles_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Consoles requests' do
+  describe 'GET /consoles' do
+    it 'returns an array of the consoles' do
+      get('/consoles')
+    #   response_json = JSON.parse(response.body)
+      expect(response_json['consoles']).to contain_exactly(
+        'NES',
+        'SNES',
+        'Wii',
+        'Genesis',
+        'Xbox',
+        'Switch',
+        'PS1',
+        'PS2'
+      )
+    end
+
+    it 'supports specifying consoles for a specific manufacturer' do
+      get('/consoles', params: { manufacturer: 'Nintendo' })
+    #   response_json = JSON.parse(response.body)
+      expect(response_json['consoles']).to contain_exactly(
+        'NES',
+        'SNES',
+        'Wii',
+        'Switch'
+      )
+    end
+  end
+end
--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Status requests" do
             get('/status')
             json = JSON.parse(response.body)
             expect(json['status']).to eql('ok')
+            expect(response.status).to eql(200)
         end
     end
 end
