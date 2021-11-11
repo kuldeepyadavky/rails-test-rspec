997572224b502a19b0e5d5dabff8257def62cdd1
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-11 16:18:15 +0530

    rspec 4

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,22 +1,22 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-class Console
-  def initialize(name, manufacturer)
-    @name = name
-    @manufacturer = manufacturer
-  end
+# class Console
+#   def initialize(name, manufacturer)
+#     @name = name
+#     @manufacturer = manufacturer
+#   end
 
-  def formatted_name
-    "#{@manufacturer} #{@name}"
-  end
-end
+#   def formatted_name
+#     "#{@manufacturer} #{@name}"
+#   end
+# end
 
-RSpec.describe Console do
-  subject { described_class.new('Switch', 'Nintendo') }
+# RSpec.describe Console do
+#   subject { described_class.new('Switch', 'Nintendo') }
 
-  describe '#formatted_name' do
-    it 'returns the manufacturer and the console name together' do
-      expect(subject.formatted_name).to eql('Nintendo Switch')
-    end
-  end
-end
+#   describe '#formatted_name' do
+#     it 'returns the manufacturer and the console name together' do
+#       expect(subject.formatted_name).to eql('Nintendo Switch')
+#     end
+#   end
+# end
